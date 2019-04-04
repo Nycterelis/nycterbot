df171225bfb2f570df742647dd4d730f269ccb3e
Reid Thompson <toomuchyang@gmail.com>
2019-04-03 22:29:32 -0400

    More testing of the GUI window, sizing, and scale

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+	"fmt"
+
+	"fyne.io/fyne/layout"
+
+	"fyne.io/fyne"
 	"fyne.io/fyne/app"
 	"fyne.io/fyne/widget"
 )
@@ -9,11 +14,43 @@ func main() {
 	app := app.New()
 
 	w := app.NewWindow("NycterBot")
-	w.SetContent(widget.NewVBox(
-		widget.NewLabel("Dummy Label 1"),
-		widget.NewButton("Quit", func() {
-			app.Quit()
-		}),
-	))
+
+	// Creating the tool bar
+	// toolbar := widget.NewToolbar()
+
+	// Creating the components of the First HBox
+	message := widget.NewMultiLineEntry()
+	message.SetReadOnly(true)
+	message.SetPlaceHolder(`This is an example of placeholding text 
+Hell yeah, Resinger is drunk af. 
+I cannot remember what linefeed is. 
+I am the worst developer ever. Who ever is my mother. I was orphaned as a young
+boy.
+Can you smell-l-l-l-l-l-l-l-l what The ROCK is cookin?`)
+	chatList := widget.NewMultiLineEntry()
+	chatList.SetReadOnly(true)
+
+	// Creating the Components of the Second HBox
+	textEntry := widget.NewEntry()
+	enterButton := widget.NewButton("Send", (func() { app.Quit() }))
+
+	// Constructing boxes for use.
+	bigTopBox := widget.NewHBox(message, chatList)
+	smallBottomBox := widget.NewHBox(textEntry, enterButton)
+	boxes := make([]fyne.CanvasObject, 0)
+	boxes = append(boxes, bigTopBox)
+	boxes = append(boxes, smallBottomBox)
+	theLayout := layout.NewHBoxLayout()
+	// layoutSize := fyne.NewSize(1000, 300)
+	// theLayout.Layout(boxes, layoutSize)
+
+	theContainer := fyne.NewContainerWithLayout(
+		theLayout, bigTopBox)
+	theContainer.AddObject(smallBottomBox)
+
+	w.SetContent(theContainer)
+
+	fmt.Print(w.Content().Size())
+
 	w.ShowAndRun()
 }
